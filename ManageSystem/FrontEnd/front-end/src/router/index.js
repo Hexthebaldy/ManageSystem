8584144f7b607cf5952de177fee8ce37fad10ea8
WenhaoYang <7thwesk@gmail.com>
2024-08-10 15:58:53 +0800

    装包

---
+++ ManageSystem/FrontEnd/front-end/src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter,createWebHashHistory } from "vue-router";
+
+const routes = [
+    {
+
+    }
+];
+
+const router = createRouter({
+    history:createWebHashHistory(),
+    routes
+});
+
+export default router;
